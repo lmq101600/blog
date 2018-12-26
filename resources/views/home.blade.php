02bfde4c514c90c0bb07a2d1ef5bd51dac46b845
home <liangmq6@163.com>
2018-12-26 23:11:11 +0800

    1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -49,44 +49,44 @@
 {{--@endsection--}}
 
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+{{--<!DOCTYPE html>--}}
+{{--<html lang="en">--}}
+{{--<head>--}}
+    {{--<meta charset="utf-8">--}}
+    {{--<meta http-equiv="X-UA-Compatible" content="IE=edge">--}}
+    {{--<meta name="viewport" content="width=device-width, initial-scale=1">--}}
 
-    <title>Laravel</title>
+    {{--<title>Laravel</title>--}}
 
-    <link href="//cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
-    <script src="//cdn.bootcss.com/jquery/2.2.4/jquery.min.js"></script>
-    <script src="//cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-</head>
+    {{--<link href="//cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">--}}
+    {{--<script src="//cdn.bootcss.com/jquery/2.2.4/jquery.min.js"></script>--}}
+    {{--<script src="//cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>--}}
+{{--</head>--}}
 
-<div id="title" style="text-align: center;">
-    <h1>Laravel</h1>
-    <div style="padding: 5px; font-size: 16px;">Laravel</div>
-</div>
-<hr>
-<div id="content">
-    <ul>
-        @foreach ($articles as $article)
-            <li style="margin: 50px 0;">
-                <div class="title">
-                    <a href="{{ url('article/'.$article->id) }}">
-                        <h4>{{ $article->title }}</h4>
-                    </a>
-                </div>
-                <div class="body">
-                    <p>{{ $article->body }}</p>
-                </div>
-            </li>
-        @endforeach
-    </ul>
-</div>
+{{--<div id="title" style="text-align: center;">--}}
+    {{--<h1>Laravel</h1>--}}
+    {{--<div style="padding: 5px; font-size: 16px;">Laravel</div>--}}
+{{--</div>--}}
+{{--<hr>--}}
+{{--<div id="content">--}}
+    {{--<ul>--}}
+        {{--@foreach ($articles as $article)--}}
+            {{--<li style="margin: 50px 0;">--}}
+                {{--<div class="title">--}}
+                    {{--<a href="{{ url('article/'.$article->id) }}">--}}
+                        {{--<h4>{{ $article->title }}</h4>--}}
+                    {{--</a>--}}
+                {{--</div>--}}
+                {{--<div class="body">--}}
+                    {{--<p>{{ $article->body }}</p>--}}
+                {{--</div>--}}
+            {{--</li>--}}
+        {{--@endforeach--}}
+    {{--</ul>--}}
+{{--</div>--}}
 
-</body>
-</html>
+{{--</body>--}}
+{{--</html>--}}
 
 
 
