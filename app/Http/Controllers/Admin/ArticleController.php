ef1e97dd959c6403bdebf6b3aa88cb0cdc44e58d
lmq <1186330636@qq.com>
2018-12-28 18:28:18 +0800

    1

--- app/Http/Controllers/Admin/ArticleController.php
+++ app/Http/Controllers/Admin/ArticleController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\Admin;
-
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Article;
@@ -15,7 +15,8 @@ class ArticleController extends Controller
 	}
 	public function create()
 	{
-		return view('admin/article/create');
+//		return view('admin/article/create');
+		return view('admin/article/add');
 	}
 	public function store(Request $request)
 	{
@@ -26,21 +27,61 @@ class ArticleController extends Controller
 		$article = new Article();
 		$article->title = $request->get('title');
 		$article->body = $request->get('body');
+		$article->keywords = $request->get('keywords');
+		$article->articlesort = $request->get('articlesort');
+		$article->articlecolumn = $request->get('articlecolumn');
+		$article->articletype = $request->get('articletype');
+//		$article->author = $request->get('author');
+		$article->allowcomments = 1;
+		$article->commentdatemin = $request->get('commentdatemin');
+		$article->commentdatemax = $request->get('commentdatemax');
 		$article->user_id = $request->user()->id;
 		if($article->save()) {
-			return redirect('admin/articles');
+//			return redirect('admin/articles');
+			return array('code'=>0,'msg'=>'保存成功');
 		} else {
-			return redirect()->back()->withInput()->withErrors('保存失败！');
+//			return redirect()->back()->withInput()->withErrors('保存失败！');
+			return array('code'=>1,'msg'=>'保存失败');
 		}
 	}
-	public function destroy($id)
+	public function destroy(Request $request)
 	{
-		return 1;
-		Article::find($id)->delete();
-		return redirect()->back()->withInput()->withErrors('删除成功！');
+		$id = $request->get('id');
+		$a = Article::find($id)->delete();
+		if($a) {
+			return array('code'=>0);
+		} else {
+			return array('code'=>1,'msg'=>'删除失败');
+		}
+//		return redirect()->back()->withInput()->withErrors('删除成功！');
 	}
 	public function edit($id)
 	{
-		
+		$articles = DB::table('articles')->where('id', $id)->first();
+		return view('admin/article/edit',['articles' => $articles]);
+	}
+	public function edit_update(Request $request)
+	{
+		$a = DB::table('articles')
+			->where('id', $request->get('id'))
+			->update([
+				'title' => $request->get('title'),
+				'body' => $request->get('body'),
+				'keywords' => $request->get('keywords'),
+				'articlesort' => $request->get('articlesort'),
+				'articlecolumn' => $request->get('articlecolumn'),
+				'articletype' => $request->get('articletype'),
+				'allowcomments' => 1,
+				'commentdatemin' => $request->get('commentdatemin'),
+				'commentdatemax' => $request->get('commentdatemax'),
+				'user_id' => $request->user()->id,
+			]);
+		if($a) {
+//			return redirect('admin/articles');
+			return array('code'=>0,'msg'=>'保存成功');
+		} else {
+//			return redirect()->back()->withInput()->withErrors('保存失败！');
+			return array('code'=>1,'msg'=>'保存失败');
+		}
 	}
 }
